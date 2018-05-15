00da94c497db001c3f0ac4e1efa6debc5828ab65
strawbinder <strawbinder@gmail.com>
2018-05-15 18:17:16 -0400

    Update .tx.sh

--- .tx.sh
+++ .tx.sh
@@ -30,7 +30,7 @@ token =" > ~/.transifexrc
 tx_push() {
   # Only run once, and only on $TX_BRANCH branch
   echo $TRAVIS_JOB_NUMBER | grep "\.1$"
-  if [ $? -eq 0 ] && [ $TRAVIS_BRANCH = $TX_BRANCH ]
+  if [ $? -eq 0 ]
     then
       tx_init
       tx push --source --no-interactive
